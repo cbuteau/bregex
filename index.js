3898dcac7418644a814236aff8a888d770b0f17d
Christopher Buteau <cbuteau@gmail.com>
2020-03-27 19:06:23 -0400

    started something

---
+++ index.js
@@ -0,0 +1,49 @@
+
+// AL third parties
+var Regex = require("regex");
+var inquirer = require('inquirer');
+var chalk = require('chalk');
+
+var current;
+
+var changeRegex = function() {
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'regex',
+      default: ''
+    }
+  ]).then(function(results) {
+    try {
+      current = new Regex(results.regex);
+    } catch(e) {
+      console.error(e);
+      console.log(chalk.red('not proepr'));
+      changeRegex();
+    }
+
+    inquirer.prompt([
+      {
+        type: 'list',
+        name: 'activity',
+        choices: [
+          'Edit',
+          'Test'
+        ]
+      }
+    ]).then(function(resultsAct) {
+      switch (resultsAct.activity) {
+        case 'Edit':
+        console.log('Edit');
+          break;
+        case 'Test':
+        console.log('Test');
+          break;
+      }
+    });
+
+  });
+
+};
+
+changeRegex();
