402f817035d69c8cbb02c1d01ceec0637bc152f8
Christopher Buteau <cbuteau@gmail.com>
2020-08-15 07:38:33 -0400

    moved to puppeteer basic test is working

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # bregex
 A regex builder
+
+## Concept
+
+A commandline tool that lets the user experiemnt with regex and test their regex.
+
+## Status
+
+### Cool Links
+
++ [Common to Test](https://digitalfortress.tech/tricks/top-15-commonly-used-regex/)
+
+### 8/15/2020
+
+Found that the regex package was faulty (did not work from basic number regex.)
+And now am trying to run with puppeteer run the regex on Chrome.
--- index.js
+++ index.js
@@ -1,10 +1,81 @@
 
+const DEBUG = false;
+
+const START_OPTS = DEBUG ? {
+  slowMo: 250,
+  headless: false,
+  devtools: true
+} : {};
+
 // AL third parties
-var Regex = require("regex");
+const puppeteer = require('puppeteer');
 var inquirer = require('inquirer');
 var chalk = require('chalk');
 
 var current;
+
+var expression;
+
+var validRegexMenu = function() {
+  inquirer.prompt([
+    {
+      type: 'list',
+      name: 'activity',
+      choices: [
+        'Test',
+        'Parse',
+        'Edit'
+      ]
+    }
+  ]).then(function(resultsAct) {
+    switch (resultsAct.activity) {
+      case 'Edit':
+        console.log('Edit');
+        break;
+      case 'Parse':
+        console.log('Parse');
+        // TODO parse expression and explain pieces.
+        break;
+      case 'Test':
+        console.log('Test');
+        inquirer.prompt([
+          {
+            type: 'input',
+            name: 'test',
+            default: ''
+          }
+        ]).then(function(results) {
+          puppeteer.launch(START_OPTS).then(function(browser) {
+            browser.newPage().then(function(page) {
+              var params = {
+                expression: expression,
+                test: results.test
+              };
+              page.evaluate((params) => {
+                //debugger;
+                var current = new RegExp(params.expression);
+                if (current.test(params.test)) {
+                  return true;
+                } else {
+                  return false;
+                }
+              }, params).then(function(regexTest) {
+                if (regexTest) {
+                  console.log('PASS test');
+                } else {
+                  console.log('FAIL test');
+                }
+                browser.close();
+                validRegexMenu();
+              });
+            });
+          });
+        });
+        break;
+    }
+  });
+};
+
 
 var changeRegex = function() {
   inquirer.prompt([
@@ -14,46 +85,47 @@ var changeRegex = function() {
       default: ''
     }
   ]).then(function(results) {
-    try {
-      current = new Regex(results.regex);
-    } catch(e) {
-      console.error(e);
-      console.log(chalk.red('not proepr'));
-      changeRegex();
-    }
+    expression = results.regex;
+    puppeteer.launch(START_OPTS).then(function(browser) {
+      var page = browser.newPage().then(function(page) {
+        page.evaluate(expression => {
+          //debugger;
+          let current;
+          try {
+            current = new RegExp(expression);
+            return true;
+          } catch (e) {
+            return false;
+          }
+        }, expression).then(function(data) {
+          if (!data) {
+            changeRegex();
+          } else {
+            validRegexMenu();
+          }
+          browser.close();
+        });
 
-    inquirer.prompt([
-      {
-        type: 'list',
-        name: 'activity',
-        choices: [
-          'Edit',
-          'Test'
-        ]
-      }
-    ]).then(function(resultsAct) {
-      switch (resultsAct.activity) {
-        case 'Edit':
-          console.log('Edit');
-          break;
-        case 'Test':
-          console.log('Test');
-          inquirer.prompt([
-            {
-              type: 'input',
-              name: 'test',
-              default: ''
-            }
-          ]).then(function(results) {
-            if (current.test(results.test)) {
-              console.log('PASS test');
-            } else {
-              console.log('FAIL test');
-            }
-          });
-          break;
-      }
+
+        // .then(function(result) {
+        //   if (!result) {
+        //     changeRegex();
+        //   } else {
+        //     expression = results.regex;
+        //     validRegexMenu();
+        //   }
+        //   browser.close();
+        // });
+      });
     });
+    // try {
+    //   current = new Regex(results.regex);
+    // } catch(e) {
+    //   console.error(e);
+    //   console.log(chalk.red('not proepr'));
+    //   changeRegex();
+    // }
+
 
   });
 
